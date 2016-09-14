5fb5f7760ce5f7f906167d5887feba3b0fd8a1b0
clairmartin <clairmartin4@gmail.com>
2016-09-14 11:48:46 -0700

    r basics

--- qbc_rscript_1.r
+++ qbc_rscript_1.r
@@ -1,5 +1,13 @@
 #this is a comment in R
 
-# to highlight and print a line in console (matlab F9)  use ctrl+return
+# to highlight and print a line in console (ml = F9)  use ctrl+return
 
-2 + 2 
+# to get working directory (ml = cd)
+getwd()
+
+# to set working direcotry (ml = cd())
+setwd("/Users/jungminson/qbc_martin")
+#N.B. double quotes not single quotes a la ml
+
+# for file naming avoid SPACES and probably - dash. Stick with under_score or per.iod
+
